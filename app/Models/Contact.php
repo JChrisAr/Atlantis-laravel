9c1e8e5c2795a01bcb6ef9a7975e3e2340c7d5e8
José Christian Arreola Álvarez <jchris.arw@gmail.com>
2025-10-20 15:00:50 -0600

    Configuración de las relaciones en los modelos.

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Contact extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'property_id', 'name', 'email', 'phone', 'message', 'contact_method'
+    ];
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
 }
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Property extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'title', 'description', 'price', 'type', 'category',
+        'address', 'city', 'state', 'bedrooms', 'bathrooms', 'size', 'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
